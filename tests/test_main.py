316e24596574f0c8d43e695e938d5eb6aafb779e
amdanalytics <amdanalyticsresearch@gmail.com>
2025-09-14 10:21:07 +0530

    sixth changes

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,6 +6,7 @@ def test_home():
     assert response.status_code == 200
     assert b"Iris Prediction" in response.data  # Check for page title
 
+
 def test_predict():
     client = app.test_client()
     response = client.post('/predict', json={'features': [5.1, 3.5, 1.4, 0.2]})
